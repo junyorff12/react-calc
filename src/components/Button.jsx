c693e6b6577c19e5fc8ae4b487d00b59ef849f2e
junyorff12 <junyorff12@gmail.com>
2022-06-09 13:01:30 -0300

    finished style of calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,15 @@
+import './Button.css';
+
 const Button = props =>
-  <button className="button">{props.label}</button>;
+  <button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+  `}
+    onClick={() => props.click && props.click(props.label)}
+  >
+    {props.label}
+  </button>;
 
 export default Button;
